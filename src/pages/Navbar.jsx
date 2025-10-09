39b8e0ef7be5975db0e08db25d50faf3e89bb5e2
Jinadev <jinad3v@gmail.com>
2025-10-09 19:30:31 +0530

    font awsome and remixicon cdn added

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    // main
+    <div>
+{/* container */}
+<div className='w-[90%] max-w-[1200px]'>
+{/* logo */}
+<div className="">
+
+</div>
+{/* links and hamburger */}
+<div className="">
+  
+</div>
+
+</div>
+    </div>
   )
 }
 
