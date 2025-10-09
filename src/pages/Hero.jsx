a241b941625aa256871ea65232f2b45d94f39397
akhilsm <primeorwott@gmail.com>
2025-10-09 19:55:21 +0530

    home secound touch

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -19,16 +19,21 @@ function Hero() {
 
   return (
     // section hero
-    <div className="h-[100vh] p-4 md:px-8 md:py-7 font-inter">
+    <div className="h-[100vh] p-4 md:px-8 md:pb-7 font-inter">
       {/* background container */}
       <div
         className="h-full w-full bg-cover bg-center text-white rounded-xl md:rounded-2xl flex items-end"
         style={{ backgroundImage: `url(${bg})` }}
       >
         {/* content */}
-        <div className="p-4 md:p-8 flex flex-col justify-end lg:flex-row gap-4 lg:items-end">
+        <div className="w-full px-4 py-6 md:p-8 flex flex-col justify-end lg:justify-between lg:flex-row gap-4 lg:items-end">
           {/* animated name */}
-          <div className="text-5xl text-[#ff9659] md:text-[100px] font-extrabold flex">
+          <div
+            className="flex text-[#ff8037] font-extrabold w-full lg:w-2/4"
+            style={{
+              fontSize: "clamp(4rem, 6vw, 9rem)", // min 2.5rem, max 9rem, grows with viewport
+            }}
+          >
             <div className="letter">G</div>
             <div className="letter">i</div>
             <div className="letter">v</div>
@@ -39,7 +44,7 @@ function Hero() {
             <div className="letter">l</div>
           </div>
 
-          <p className="text-lg md:text-xl tracking-wide">
+          <p className="text-lg md:text-xl font-semibold tracking-wide w-full lg:w-1/4">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Voluptatibus maiores, ipsa natus ab cupiditate.
           </p>
